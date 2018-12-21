f8e005b5dbc5ee371dbf1b2cdd5cda19215955dc
jay bro <jason.brown@it-checkout.co.uk>
2018-12-21 05:23:27 +0000

    Fixed Bugs

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
 const SALT_I = 10;
 require('dotenv').config();
@@ -50,7 +50,7 @@ userSchema.pre('save',function(next){
         bcrypt.genSalt(SALT_I,function(err,salt){
             if(err) return next(err);
     
-            bcrypt.hash(user.password,salt,function(err,hash){
+            bcrypt.hash(user.password,salt,null,function(err,hash){
                 if(err) return next(err);
                 user.password = hash;
                 next();
